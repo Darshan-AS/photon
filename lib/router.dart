7dd8a1d50890c3f9e7444dc5b413904ce9bffef5
Darshan A S <itisdarshan@gmail.com>
2020-01-08 02:32:45 +0530

    Add borrower detail page

--- lib/router.dart
+++ lib/router.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:photon/screens/borrower/borrower.dart';
 import 'package:photon/screens/home/home.dart';
 
 class Router {
   static const String homeRoute = '/';
+  static const String borrowerRoute = '/borrower';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case homeRoute:
         return MaterialPageRoute(builder: (_) => Home());
+      case borrowerRoute:
+        return MaterialPageRoute(builder: (_) => Borrower());
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
---
+++ lib/screens/borrower/borrower.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Borrower extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Borrower Detail')),
+      body: Center(child: Text('Borrower detail page')),
+    );
+  }
+}
