f5b013ec95b62971127f06bbbdbdc8affdcaa1a9
Darshan A S <itisdarshan@gmail.com>
2020-01-09 22:15:08 +0530

    Create Borrower widget

--- lib/screens/borrower/borrower.dart
+++ lib/screens/borrower/borrower.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:photon/screens/borrower/profile.dart';
 
 class Borrower extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Borrower Detail')),
-      body: Center(child: Text('Borrower detail page')),
+      appBar: AppBar(
+        title: Text('Borrower Detail'),
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      body: Column(
+        children: <Widget>[
+          Profile(),
+          Divider(color: Colors.black),
+        ],
+      ),
     );
   }
 }
